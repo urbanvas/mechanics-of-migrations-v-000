30e6d5a6233baff0a59444006ae75798e523826e
Neil Seejoor <nvseejoor@gmail.com>
2018-04-04 19:15:01 +0000

    automatically backed up by learn

--- db/01_create_artists.rb
+++ db/01_create_artists.rb
@@ -1,9 +1,9 @@
 class CreateArtists < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
+  # def up
+  # end
+  #
+  # def down
+  # end
 
   def change
     create_table :artists do |t|
